c2a1f869931600a76d7a80ba0c21ab8334cb7200
Jordan <jkimys2@gmail.com>
2024-02-10 17:59:13 -0500

    server file connect to db

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // Import modules
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+require("dotenv").config;
 
 // Initialize port
 const PORT = process.env.PORT || 3001;
 
 // Connect to database
-const db = mysql.createConnection({
-    
-}
-)
+const db = mysql.createConnection(
+  {
+    host: DBHOST,
+    user: DBUSER,
+    password: DBPASSWORD,
+    database: "employees_db",
+  },
+  console.log(`Connected to employees_db database!`)
+);
