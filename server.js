2e7e08cfec4ff5c920edc845b16a075cbfa293f8
Jordan <jkimys2@gmail.com>
2024-02-10 17:51:21 -0500

    server file update

---
+++ server.js
@@ -0,0 +1,12 @@
+// Import modules
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+// Initialize port
+const PORT = process.env.PORT || 3001;
+
+// Connect to database
+const db = mysql.createConnection({
+    
+}
+)
