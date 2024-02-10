8a39eefa788b525d3e7f5538d52fb22d83e5058d
Jordan <jkimys2@gmail.com>
2024-02-10 17:45:29 -0500

    seeds file update

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,4 +16,16 @@ VALUES
     ("Senior Attorney", "175,000", 3),
     ("Junior Attorney", "130,000", 3),
     ("Sales Manager", "120,000", 4),
-    ("Sales Associate", "75,000", 4),
+    ("Sales Associate", "75,000", 4);
+
+INSERT INTO
+    employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Miranda", "Bailey", 5),
+    ("Meredith", "Grey", 1),
+    ("Alex", "Karev", 8, 7),
+    ("George", "O'Malley", 6, 1),
+    ("Derek", "Shepherd", 4, 8),
+    ("Izzie", "Stephens", 2, 2),
+    ("Richard", "Webber", 7),
+    ("Christina", "Yang", 3);
