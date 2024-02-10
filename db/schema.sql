ad7b2ee7c863dbdbd3f4b71cc5e17406372b0f2f
Jordan <jkimys2@gmail.com>
2024-02-10 17:32:05 -0500

    seeds file update'

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary: DECIMAL NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
@@ -30,4 +30,4 @@ CREATE TABLE employee (
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
-)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+    department (department_name)
+VALUES
+    ("Engineering"),
+    ("Finance"),
+    ("Legal"),
+    ("Sales");
+
+INSERT INTO
+    roles (title, salary, department_id)
+VALUES
+    ("Engineering Manager", "140,000", 1),
+    ("Software Engineer", "120,000", 1),
+    ("Finance Manager", "150,000", 2),
+    ("Accountant", "120,000", 2),
+    ("Senior Attorney", "175,000", 3),
+    ("Junior Attorney", "130,000", 3),
+    ("Sales Manager", "120,000", 4),
+    ("Sales Associate", "75,000", 4),
