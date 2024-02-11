8e96626adb372bd7ee8ed52683e2ce2f6e3343dc
Jordan <jkimys2@gmail.com>
2024-02-10 21:13:58 -0500

    view dept function

--- server.js
+++ server.js
@@ -68,8 +68,20 @@ const mainPrompt = () => {
     });
 };
 
+// Function to view all the departments
+const viewAllDept = () => {
+    const sql = "SELECT * FROM department;";
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.table(result);
+      mainPrompt();
+    });
+  };
 
-
+ 
 // Function to exit prompts
 const exit = () => {
   console.log("Goodbye!");
