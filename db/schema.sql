e765659d6dd2748f733215618e4b20f8e96569a8
Jordan <jkimys2@gmail.com>
2024-02-13 18:22:48 -0500

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -27,5 +27,4 @@ CREATE TABLE
         role_id INT,
         manager_id INT,
         FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE SET NULL
-        FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
     );
